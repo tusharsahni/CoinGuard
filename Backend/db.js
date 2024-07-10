a45364f7392ac989fd13c86e8ad7bb29cd8652e5
devanshvashisht <devanshvashisht22@gmail.com>
2024-07-10 23:29:04 +0530

    created backend folder

--- Backend/db.js
+++ Backend/db.js
@@ -2,7 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  connectionString: "postgresql://KuwarJ:Pass123@localhost:5432/CoinGuard",
+  connectionString: "postgresql://devanshv:Pass123@localhost:5432/CoinGuard",
 });
 
 module.exports = pool;
--- Backend/transactions/index.js
+++ Backend/transactions/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const pool = require("./db");
+const pool = require("../db");
 
 const PORT = process.env.PORT || 3000;
 
